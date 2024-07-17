5bdc3125b8633f47919848687328218e20b8e4dc
Pascal Nehlsen <pascaldev92@gmail.com>
2024-07-17 14:35:09 +0200

    added first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './landingPage/landingPage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'sakura';
---
+++ src/app/landingPage/landingPage.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-landingPage',
+  standalone: true,
+  imports: [CommonModule],
+  template: ` <section class="fontRaleway">
+    <h1>SAKURA RAMEN</h1>
+    <h2>BEST RAMEN IN TOWN</h2>
+  </section>`,
+  styleUrls: ['./landingPage.component.scss'],
+})
+export class LandingPageComponent {}
